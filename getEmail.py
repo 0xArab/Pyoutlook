27e1e274ecf97c066cfea10b48b97816c51d9f16
0xArab <46280856+0xArab@users.noreply.github.com>
2021-02-19 21:25:24 +0300

    Update getEmail.py

--- getEmail.py
+++ getEmail.py
@@ -17,65 +17,68 @@ except ImportError:
 """
 class outlook:
         """  
-             - Main informations  
-               We want to make this variables available to everyone. 
+           - Main informations  
+             We want to make this variables available to everyone. 
         """
-
         SESSION = requests.Session()
         URL_OUTLOOK = "https://login.live.com/"
         UUID_HEADER = ""
         URL_LOGIN = "https://login.live.com/GetCredentialType.srf?uaid="
 
-
         def __init__(self,Email):
-              self.Email_outlook = Email
+            self.Email_outlook = Email
                     
         """ set uuid Because we want to use it again."""
-        def set_uuid():
-                self.request_uuid = self.SESSION.get(URL_OUTLOOK)
-                self.UUID_HEADER = self.request_uuid.cookies['uaid']
+        def set_uuid():  
+            self.request_uuid = self.SESSION.get(URL_OUTLOOK)
+            self.UUID_HEADER = self.request_uuid.cookies['uaid']
+            if self.UUID_HEADER:
+               return True
+            else:
+               return False
 
-                if self.UUID_HEADER:
-                     return True
-                else:
-                     return False
-
-       """  universally unique identifier 
+       """  
+           universally unique identifier 
                - UUID) is a 128-bit number used to identify information in computer systems. The term globally unique identifier (GUID) is also used
                - When generated according to the standard methods, UUIDs are for practical purposes unique
                - Their uniqueness does not depend on a central registration authority or coordination between the parties generating them, unlike most other numbering schemes
                - anyone can create a UUID and use it to identify something with near certainty that the identifier does not duplicate one that has already been, or will be, created to identify something else. Information labeled with UUIDs by independent parties can therefore
        """
         def get_uuid(self):
-                return self.UUID_HEADER
+            return self.UUID_HEADER
 
         """ - This is Microsoft Flow - Token Authentication """
         def get_flow(self):
-                return re.findall('<input type="hidden" name="PPFT" id="i0327" value=(.*?)>',self.request_uuid.text)[0].replace('""/','').replace('"',"")
+            return re.findall('<input type="hidden" name="PPFT" id="i0327" value=(.*?)>',self.request_uuid.text)[0].replace('""/','').replace('"',"")
         
         def data_requests():
-                 return  {"username":self.Email_outlook,"uaid":self.get_uuid(),"isOtherIdpSupported":"false","checkPhones":"false","isRemoteNGCSupported":"true","isCookieBannerShown":"false",
-                   "isFidoSupported":"false",
-                         "forceotclogin":"false",
-                    "otclogindisallowed":"false",
+                 return  {"username":self.Email_outlook,
+                          "uaid":self.get_uuid(),
+                          "isOtherIdpSupported":"false",
+                          "checkPhones":"false",
+                          "isRemoteNGCSupported":"true",
+                          "isCookieBannerShown":"false",
+                          "isFidoSupported":"false",
+                          "forceotclogin":"false",
+                          "otclogindisallowed":"false",
                           "isExternalFederationDisallowed":"false",
-                   "isRemoteConnectSupported":"false",
+                          "isRemoteConnectSupported":"false",
                           "federationFlags":3,
-                   "isSignup":"false",
+                          "isSignup":"false",
                           "flowToken":self.get_flow()
                         }
+             
         """ Login and get data of others functions """
-
         def Login_to_outlook(self):
-                get_uuid_and_url_login = self.URL_LOGIN + str ( self.get_uuid() )
-                return session.post(URL_LOGIN,json=self.data_requests()).text
-
+            get_uuid_and_url_login = self.URL_LOGIN + str ( self.get_uuid() )
+            return session.post(URL_LOGIN,json=self.data_requests()).text
+        
        """ We check the content that's in function Login_to_outlook  """ 
         def respone_of_login(self)
-                if '"IfExistsResult":0' in self.Login_to_outlook():
-                        print( "   This Email exists  !" )
-                else:
-                        print("[+++++++++] Email Not exists ")
+            if '"IfExistsResult":0' in self.Login_to_outlook():
+                print( "   This Email exists  !" )
+            else:
+                print("[+++++++++] Email Not exists ")
                 
 
         
